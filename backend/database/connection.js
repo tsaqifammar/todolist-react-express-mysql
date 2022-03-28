e3228aedd2362c84aa2cf56bf0c8a1e39581395a
Muhammad Tsaqif Ammar <54428874+tsaqifammar@users.noreply.github.com>
2022-03-28 15:59:42 +0700

    Add database pooling

--- backend/database/connection.js
+++ backend/database/connection.js
@@ -1,7 +1,10 @@
 const mysql = require('mysql2');
 
-module.exports = mysql.createConnection({
+module.exports = mysql.createPool({
   host: 'localhost',
   user: 'root',
   database: 'to-do-list-app',
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
 });
