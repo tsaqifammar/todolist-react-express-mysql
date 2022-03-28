d2dbc6353d7e13cd112bf0f2a009419f390e727b
Muhammad Tsaqif Ammar <54428874+tsaqifammar@users.noreply.github.com>
2022-03-28 16:19:37 +0700

    Add logger middleware

--- backend/server.js
+++ backend/server.js
@@ -4,10 +4,13 @@ const cors = require('cors');
 
 const app = express();
 
-const PORT = process.env.PORT || 4000;
+const logger = require('./middlewares/logger');
 
 app.use(express.json());
 app.use(cors());
+app.use(logger);
+
+const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
