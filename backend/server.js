c59dfa7a234398d2c07b0787f2347b84b3149ce7
Muhammad Tsaqif Ammar <54428874+tsaqifammar@users.noreply.github.com>
2022-03-28 16:05:13 +0700

    Adjust setup to server

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,13 @@
 const express = require('express');
 
+const cors = require('cors');
+
 const app = express();
 
 const PORT = process.env.PORT || 4000;
+
+app.use(express.json());
+app.use(cors());
 
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
