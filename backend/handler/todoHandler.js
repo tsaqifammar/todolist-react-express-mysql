8aa0ad170d60bb158c819000ea87c116edaecdab
Muhammad Tsaqif Ammar <54428874+tsaqifammar@users.noreply.github.com>
2022-03-28 18:10:21 +0700

    Add updateSection route

--- backend/handler/todoHandler.js
+++ backend/handler/todoHandler.js
@@ -27,6 +27,30 @@ function createSection(req, res) {
   );
 }
 
+function updateSection(req, res) {
+  const { id } = req.params;
+  const { name } = req.body;
+
+  if (name === undefined) {
+    return res.status(400).json({
+      message: 'Failed updating section. Please provide a new name',
+    });
+  }
+
+  db.execute(
+    'UPDATE section SET name = ? WHERE id = ?',
+    [name, id],
+    (err, results) => {
+      if (err) return res.status(400).json({ message: 'Failed updating section' });
+      if (results.affectedRows === 0) {
+        return res.status(404).json({ message: 'Failed updating section. Id not found' });
+      }
+      return res.status(200).json({ message: 'Section successfully updated' });
+    },
+  );
+}
+
 module.exports = {
   createSection,
+  updateSection,
 };
--- backend/routes/todo.js
+++ backend/routes/todo.js
@@ -5,5 +5,6 @@ const router = express.Router();
 const todoHandler = require('../handler/todoHandler');
 
 router.post('/sections', todoHandler.createSection);
+router.put('/sections/:id', todoHandler.updateSection);
 
 module.exports = router;
