b5a3f76bd5eb7372aee601450dffdad92d1a96e3
Muhammad Tsaqif Ammar <54428874+tsaqifammar@users.noreply.github.com>
2022-03-31 22:06:03 +0700

    Add preservation of url params

--- frontend/src/components/Todo/index.jsx
+++ frontend/src/components/Todo/index.jsx
@@ -1,10 +1,11 @@
 import { useState } from 'react';
 import PropTypes from 'prop-types';
-import { Link } from 'react-router-dom';
+import { Link, useSearchParams } from 'react-router-dom';
 import { toggleTodo } from './processData';
 
 function Todo({ data }) {
   const [isDone, setIsDone] = useState(data.is_done);
+  const [urlParams] = useSearchParams();
   
   const handleOnChange = () => {
     setIsDone((c) => !c);
@@ -14,7 +15,7 @@ function Todo({ data }) {
   return (
     <div className="p-3 border-2 border-gray-300 rounded flex items-center">
       <input type="checkbox" checked={isDone} className="" onChange={handleOnChange} />
-      <Link to={`/todo/${data.id}/edit`} className="ml-4 w-full">
+      <Link to={`/todo/${data.id}/edit?${urlParams}`} className="ml-4 w-full">
         <div className={`${isDone ? 'line-through text-gray-700' : ''}`}>{data.name}</div>
       </Link>
     </div>
